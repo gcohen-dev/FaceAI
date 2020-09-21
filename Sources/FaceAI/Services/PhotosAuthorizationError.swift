f5258ea28e9f41da5d4db25b81521e2997759dbd
amir lahav <lahavamir27@gmail.com>
2020-09-21 14:53:00 +0300

    FaceAI - add photo service

---
+++ Sources/FaceAI/Services/PhotosAuthorizationError.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosAuthorizationError.swift
+//  
+//
+//  Created by amir.lahav on 21/09/2020.
+//
+
+import Foundation
+
+enum PhotosAuthorizationError: Error {
+    case denied
+    case notDetermined
+}
---
+++ Sources/FaceAI/Services/PhotosAuthorizationService.swift
@@ -0,0 +1,38 @@
+//
+//  PhotosAuthorizationService.swift
+//  FaceAI
+//
+//  Created by amir.lahav on 21/09/2020.
+//
+
+import Photos
+
+final class PhotosAuthorizationService {
+    static func checkPhotoLibraryPermission(completion: @escaping (Result<Void, PhotosAuthorizationError>) -> Void) {
+        let status = PHPhotoLibrary.authorizationStatus()
+        switch status {
+        case .authorized, .limited: completion(Result.success(()))
+        //handle authorized status
+        case .denied, .restricted : completion(Result.failure(.denied))
+        //handle denied status
+        case .notDetermined:
+            // ask for permissions
+            PHPhotoLibrary.requestAuthorization { status in
+                DispatchQueue.main.async {
+                    switch status {
+                    case .authorized, .limited: completion(Result.success(()))
+                    // as above
+                    case .denied, .restricted: completion(Result.failure(.denied))
+                    // as above
+                    case .notDetermined: completion(Result.failure(.notDetermined))
+                        fatalError("should not be here \(PhotosAuthorizationService.self)")
+                    @unknown default:
+                        fatalError("should not be here \(PhotosAuthorizationService.self)")
+                    }
+                }
+            }
+        @unknown default:
+            fatalError("should not be here \(PhotosAuthorizationService.self)")
+        }
+    }
+}
